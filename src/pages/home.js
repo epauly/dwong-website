8ae2bb3c8d0488c2c76ae74e28ca73fc88ba10db
Eric Pauly <esp0017@auburn.edu>
2022-03-17 18:01:03 -0700

    Added info section content in home body

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/HeroSection/HeroSection";
+import InfoSection from "../components/InfoSection/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +17,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
     </>
   );
 };
